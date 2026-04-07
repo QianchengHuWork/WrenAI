f487518c721a72e7838dce3d7618f98bbf16f08a
JayKuran <jaykuran@JayKurandeMacBook-Air.local>
2026-04-07 14:14:51 +0800

    only_show_denodo_sql

--- wren-ui/src/components/pages/home/promptThread/ViewSQLTabContent.tsx
+++ wren-ui/src/components/pages/home/promptThread/ViewSQLTabContent.tsx
@@ -2,23 +2,12 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useEffect } from 'react';
 import styled from 'styled-components';
-import {
-  Button,
-  Divider,
-  Empty,
-  message,
-  Space,
-  Switch,
-  Typography,
-} from 'antd';
-import CheckOutlined from '@ant-design/icons/CheckOutlined';
-import CloseOutlined from '@ant-design/icons/CloseOutlined';
+import { Button, Empty, Typography } from 'antd';
 import CodeFilled from '@ant-design/icons/CodeFilled';
 import { BinocularsIcon } from '@/utils/icons';
 import { nextTick } from '@/utils/time';
 import useNativeSQL from '@/hooks/useNativeSQL';
 import { DATA_SOURCE_OPTIONS } from '@/components/pages/setup/utils';
-import { Logo } from '@/components/Logo';
 import { Props as AnswerResultProps } from '@/components/pages/home/promptThread/AnswerResult';
 import usePromptThreadStore from '@/components/pages/home/promptThread/store';
 import PreviewData from '@/components/dataPreview/PreviewData';
@@ -54,6 +43,8 @@ export default function ViewSQLTabContent(props: AnswerResultProps) {
     onError: (error) => console.error(error),
   });
 
+  const { id, sql } = threadResponse;
+
   const onPreviewData = async () => {
     await previewData({ variables: { where: { responseId: id } } });
   };
@@ -72,94 +63,45 @@ export default function ViewSQLTabContent(props: AnswerResultProps) {
     }
   }, [isLastThreadResponse]);
 
-  const { id, sql } = threadResponse;
+  // Auto-fetch native SQL on mount
+  useEffect(() => {
+    fetchNativeSQL({ variables: { responseId: id } });
+  }, [id]);
 
-  const { hasNativeSQL, dataSourceType } = nativeSQLResult;
-  const showNativeSQL = hasNativeSQL;
+  const { dataSourceType } = nativeSQLResult;
 
+  // Always show native SQL (data source SQL)
   const sqls =
-    nativeSQLResult.nativeSQLMode && nativeSQLResult.loading === false
+    nativeSQLResult.loading === false && nativeSQLResult.data
       ? nativeSQLResult.data
       : sql;
-
-  const onChangeNativeSQL = async (checked: boolean) => {
-    nativeSQLResult.setNativeSQLMode(checked);
-    checked && fetchNativeSQL({ variables: { responseId: id } });
-  };
-
-  const onCopy = () => {
-    if (!nativeSQLResult.nativeSQLMode) {
-      message.success(
-        <>
-          你复制的是 Wren SQL。该方言用于 Wren
-          Engine，可能无法直接在你的数据库中运行。
-          {hasNativeSQL && (
-            <>
-              请点击“<b>显示原始 SQL</b>”获取可执行版本。
-            </>
-          )}
-        </>,
-      );
-    }
-  };
 
   return (
     <div className="text-md gray-10 p-6 pb-4">
       <StyledPre className="p-0 mb-3">
         <StyledToolBar className="d-flex align-center justify-space-between text-family-base">
           <div>
-            {nativeSQLResult.nativeSQLMode ? (
-              <>
-                <Image
-                  className="mr-2"
-                  src={DATA_SOURCE_OPTIONS[dataSourceType].logo}
-                  alt={DATA_SOURCE_OPTIONS[dataSourceType].label}
-                  width="22"
-                  height="22"
-                />
-                <Text className="gray-8 text-medium text-sm">
-                  {DATA_SOURCE_OPTIONS[dataSourceType].label}
-                </Text>
-              </>
-            ) : (
-              <span className="d-flex align-center gx-2">
-                <Logo size={18} />
-                <Text className="gray-8 text-medium text-sm">Wren SQL</Text>
-              </span>
-            )}
+            <Image
+              className="mr-2"
+              src={DATA_SOURCE_OPTIONS[dataSourceType].logo}
+              alt={DATA_SOURCE_OPTIONS[dataSourceType].label}
+              width="22"
+              height="22"
+            />
+            <Text className="gray-8 text-medium text-sm">
+              {DATA_SOURCE_OPTIONS[dataSourceType].label}
+            </Text>
           </div>
-          <Space split={<Divider type="vertical" className="m-0" />}>
-            {showNativeSQL && (
-              <div
-                className="d-flex align-center cursor-pointer"
-                onClick={() =>
-                  onChangeNativeSQL(!nativeSQLResult.nativeSQLMode)
-                }
-              >
-                <Switch
-                  checkedChildren={<CheckOutlined />}
-                  unCheckedChildren={<CloseOutlined />}
-                  className="mr-2"
-                  size="small"
-                  checked={nativeSQLResult.nativeSQLMode}
-                  loading={nativeSQLResult.loading}
-                />
-                <Text className="gray-8 text-medium text-base">
-                  显示原始 SQL
-                </Text>
-              </div>
-            )}
-            <Button
-              type="link"
-              data-ph-capture="true"
-              data-ph-capture-attribute-name="view_sql_copy_sql"
-              icon={<CodeFilled />}
-              size="small"
-              onClick={() => onOpenAdjustSQLModal({ sql, responseId: id })}
-            >
-              调整 SQL
-            </Button>
-          </Space>
+          <Button
+            type="link"
+            data-ph-capture="true"
+            data-ph-capture-attribute-name="view_sql_copy_sql"
+            icon={<CodeFilled />}
+            size="small"
+            onClick={() => onOpenAdjustSQLModal({ sql, responseId: id })}
+          >
+            调整 SQL
+          </Button>
         </StyledToolBar>
         <SQLCodeBlock
           code={sqls}
@@ -167,7 +109,6 @@ export default function ViewSQLTabContent(props: AnswerResultProps) {
           maxHeight="300"
           loading={nativeSQLResult.loading}
           copyable
-          onCopy={onCopy}
         />
       </StyledPre>
       <div className="mt-6">
